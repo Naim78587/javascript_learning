08abf242717e47ce3a899e853f9aa095194826a5
Naim78587 <naimahmed1921@gmail.com>
2024-12-10 16:32:47 +0600

    basic of js

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+//printing in JS
 console.log("I am learning JS");
 document.write("Name: Naim Ahmed <br>");
 document.write("ID: 011201050 <br>");
+//library functions and variable
+var num=35;
+var num1=40;
+var num2="2025";
+var num3=3.14162516856;
+num1=toString(num1);
+num2=parseInt(num2);
+document.write(num3.toFixed(2));
+var name="Naim Ahmed";
+document.write(name);
+name = "Emon";
+document.write(name);
+document.write(num);
+//string function
+var fname="Jhon";
+var lname="Doe";
+var name="ikhtiar uddin muhammad bakhtiyar khilji";
+var len=name.length;
+var num=30
+var fullname=fname+lname;
+document.write("Naim "+"Ahmed");
+document.write("My name is "+fullname);
+document.write("The numbe is "+num);
+document.write("The numbe is "+num);
+document.write("<br>");
+document.write(len);
+var msg=prompt("Enter any message: ");
+document.write(msg+"<br>");
+document.write("String Size: "+msg.length+"<br>");
+document.write("nth character: "+msg.charAt(3)+"<br>");
+var country="Bangladesh";
+document.write("Uppercase: "+country.toUpperCase()+"<br>");
+var m1="Java Script ";
+var m2="is beautiful";
+document.write(m1.concat(m2));
+document.write(m1.slice(0,4));
+
+
